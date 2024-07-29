7a322cc47affb077097d53facaf186ba38e018ad
Zaklekta <zaklekta@gmail.com>
2024-07-29 15:19:58 +0000

    add styles

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -6,7 +6,7 @@ const Description = () => {
       <h1 className={css["title"]}>Sip Happens Café</h1>
       <p className={css["text"]}>
         Please leave your feedback about our service by selecting one of the
-        options below.
+        options below
       </p>
     </>
   );
